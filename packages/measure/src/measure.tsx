12a147442670bfe16e2ba4e539aab2078a8040f3
kitsuyui <kitsuyui@kitsuyui.com>
2023-08-26 01:23:24 +0900

    Measure
    Defines a component for measuring.
    It is useful for debugging.

---
+++ packages/measure/src/measure.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useMeasure } from 'react-use'
+
+export const Measure: React.FC = (): JSX.Element => {
+  const [ref, { width, height }] = useMeasure<HTMLDivElement>()
+  const text = `${width}x${height}`
+  const textSize = text.length
+  const fontSize = Math.min(width, height) / textSize
+  return (
+    <div
+      ref={ref}
+      style={{
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+        overflow: 'hidden',
+      }}
+    >
+      <svg width={width} height={height}>
+        <line
+          x1={0}
+          y1={0}
+          x2={width / 2}
+          y2={height / 2}
+          stroke="black"
+          strokeWidth={1}
+          strokeDasharray={2}
+        />
+        <line
+          x1={width}
+          y1={0}
+          x2={width / 2}
+          y2={height / 2}
+          stroke="black"
+          strokeWidth={1}
+          strokeDasharray={2}
+        />
+        <line
+          x1={0}
+          y1={height}
+          x2={width / 2}
+          y2={height / 2}
+          stroke="black"
+          strokeWidth={1}
+          strokeDasharray={2}
+        />
+        <line
+          x1={width}
+          y1={height}
+          x2={width / 2}
+          y2={height / 2}
+          stroke="black"
+          strokeWidth={1}
+          strokeDasharray={2}
+        />
+        <text
+          x={width / 2}
+          y={height / 2}
+          textAnchor="middle"
+          dominantBaseline="middle"
+          fontSize={fontSize}
+          fontFamily="monospace"
+        >
+          {text}
+        </text>
+      </svg>
+    </div>
+  )
+}
