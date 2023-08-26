7224da19d3f5bef6c6517f4c7364cb17c9ee4dd3
kitsuyui <kitsuyui@kitsuyui.com>
2023-08-27 00:49:43 +0900

    ignore pnpm version from-git error

--- pnpm.links.sh
+++ pnpm.links.sh
@@ -1,38 +1,35 @@
 #!/usr/bin/env bash
+
+pnpm link ./config/eslint-config
+
 # components
 components=(clock timer stopwatch dekamoji measure treemap)
-
-# @kitsuyui/react-components depends each components
-cd ./packages/components
-for component in "${components[@]}"
-do
-  pnpm link "../../packages/$component"
-done
-cd -
-
-# @kitsuyui/react-style-bulma depends @kitsuyui/react-components and each components
-cd ./packages/style-bulma
-pnpm link ../../packages/components
-for component in "${components[@]}"
-do
-  pnpm link "../../packages/$component"
-done
-cd -
-
-# examples depends each components
-cd ./examples/storybook
-for component in "${components[@]}"
-do
-  pnpm link "../../packages/$component"
-done
-cd -
-
-# project root depends @kitsuyui/react-style-bulma and @kitsuyui/react-components and each components
-pnpm link ./packages/style-bulma
-pnpm link ./packages/components
-for component in "${components[@]}"
-do
+for component in "${components[@]}"; do
   pnpm link "./packages/$component"
 done
 
-pnpm link ./config/eslint-config-custom
+# @kitsuyui/react-components
+cd ./packages/components
+for component in "${components[@]}"; do
+  pnpm link "../../packages/$component"
+done
+cd -
+pnpm link ./packages/components
+
+# @kitsuyui/react-style-bulma
+cd ./packages/style-bulma
+pnpm link ../../packages/components
+for component in "${components[@]}"; do
+  pnpm link "../../packages/$component"
+done
+cd -
+pnpm link ./packages/style-bulma
+
+# examples depends each components
+cd ./examples/storybook
+for component in "${components[@]}"; do
+  pnpm link "../../packages/$component"
+done
+pnpm link ../../packages/components
+pnpm link ../../packages/style-bulma
+cd -
