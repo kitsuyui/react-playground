6347831feb14e6c3729594d3344f720f8156f68c
kitsuyui <kitsuyui@kitsuyui.com>
2023-09-20 02:09:58 +0900

    Dekamoji is a React component that displays a very large character.
    Improvement point: When displaying a long sentence, the size of the character is automatically adjusted.
    Until now, the calculation was centered on Japanese full-width characters.
    
    Also, when the sentence is long, it was not possible to make the characters larger by wrapping them.
    This time, I will solve this problem.

--- packages/dekamoji/src/dekamoji.tsx
+++ packages/dekamoji/src/dekamoji.tsx
@@ -1,33 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useMeasure } from 'react-use'
 
 type Props = {
   text: string
+  iterations?: number
 }
 
-export const Dekamoji: React.FC<Props> = ({ text }): JSX.Element => {
+export const Dekamoji: React.FC<Props> = ({
+  text,
+  iterations,
+}: {
+  text: string
+  iterations?: number
+}): JSX.Element => {
+  const maxIterations = iterations ?? 20
   const [ref, { width, height }] = useMeasure<HTMLDivElement>()
-  const fontSize = Math.min(height, width / Math.max(text.length, 1))
+  const maxFontSize = Math.max(width, height)
+  const [fontSize, setFontSize] = useState<number>(maxFontSize)
+  const ref2 = React.useRef<HTMLDivElement>(null)
+  const [iter, setIter] = useState<number>(0)
+
+  function calcFontSize() {
+    if (!ref2.current) return
+    if (iter === 0) return
+    const { current } = ref2
+    const overflowHeight = current.scrollHeight - 1 - height
+    const overflowWidth = current.scrollWidth - 1 - width
+    // If the text is too large, reduce the font size
+    if (overflowHeight >= 0 || overflowWidth >= 0) {
+      setFontSize(Math.max(fontSize - iter, 0))
+    }
+    // Don't expand font size at the last iteration
+    if (iter > 1) {
+      // If the text is too small, increase the font size
+      if (overflowHeight <= 0 && overflowWidth <= 0) {
+        setFontSize(Math.min(fontSize + iter, maxFontSize))
+      }
+    }
+    setIter(iter - 1)
+  }
+
+  useEffect(() => {
+    setIter(maxIterations)
+    calcFontSize()
+  }, [text, width, height, fontSize, maxFontSize])
+
   return (
     <div
       ref={ref}
       style={{
         width: '100%',
         height: '100%',
+        boxSizing: 'border-box',
         overflow: 'hidden',
       }}
     >
-      <svg width="100%" height="100%" viewBox={`0 0 ${width} ${height}`}>
-        <text
-          x="50%"
-          y="50%"
-          dominantBaseline="middle"
-          textAnchor="middle"
-          fontSize={`${fontSize}px`}
-        >
-          {text}
-        </text>
-      </svg>
+      <div
+        ref={ref2}
+        style={{
+          fontSize: fontSize + 'px',
+          textAlign: 'center',
+          margin: 'auto 0',
+          whiteSpace: 'pre-wrap',
+        }}
+      >
+        {text}
+      </div>
     </div>
   )
 }
