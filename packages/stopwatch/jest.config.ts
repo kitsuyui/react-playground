6aa96b13f0f44e794d4c9adc56a84a5d2021dbc7
kitsuyui <kitsuyui@kitsuyui.com>
2023-09-21 02:07:43 +0900

    Share jest.config.ts in monorepo

--- packages/stopwatch/jest.config.ts
+++ packages/stopwatch/jest.config.ts
@@ -1,19 +1,2 @@
 // https://jestjs.io/docs/configuration
-export default {
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+.(t|j)sx?$': [
-      '@swc/jest',
-      {
-        sourceMaps: true,
-        jsc: {
-          parser: {
-            syntax: 'typescript',
-          },
-        },
-      },
-    ],
-  },
-}
+export default require('@kitsuyui/jest-config')
