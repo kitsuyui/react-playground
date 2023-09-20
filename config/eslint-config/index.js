85ae7d643a2ed7e3243969082b6246c901963fbf
kitsuyui <kitsuyui@kitsuyui.com>
2023-09-21 00:56:32 +0900

    Each package install config/eslint-config/index.js of monorepo

--- config/eslint-config/index.js
+++ config/eslint-config/index.js
@@ -19,7 +19,16 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  ignorePatterns: ['node_modules/', 'dist/', 'build/', 'storybook-static/'],
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    'storybook-static/',
+    'coverage/',
+    '__mocks__/',
+    'jest.config.ts',
+    'tsup.config.ts',
+  ],
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
     'react-hooks/exhaustive-deps': 'error',
