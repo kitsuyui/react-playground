4eb90ec1217ecbdcad9f34725da679b212680968
kitsuyui <kitsuyui@kitsuyui.com>
2023-10-22 01:08:29 +0900

    Add tests

--- packages/editablelabel/src/test.tsx
+++ packages/editablelabel/src/test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React, { useState } from 'react'
 
-import { EditableLabel } from './EditableLabel'
+import { EditableLabel } from './index'
 
 const EditableLabelExample = () => {
   const [text, setText] = useState("Let's edit me!")
@@ -13,10 +14,23 @@ const EditableLabelExample = () => {
   )
 }
 
-test('render EditableLabel', () => {
+test('render EditableLabel', async () => {
   render(<EditableLabelExample />)
+  // view mode (initial state)
   const element = screen.getByText("Let's edit me!")
-  expect(element).toBeInstanceOf(HTMLSpanElement)
+  expect(element.textContent).toBe("Let's edit me!")
+  expect(element.tagName).toBe('SPAN')
 
-  // TODO: test edit mode
+  // click to edit
+  await userEvent.click(element)
+  await screen.findByDisplayValue("Let's edit me!")
+  const element2 = screen.getByDisplayValue("Let's edit me!")
+  expect(element2.tagName).toBe('INPUT')
+  await userEvent.type(element2, 'Hello World!')
+  // press enter to submit
+  await userEvent.type(element2, '{enter}')
+
+  // back to view mode
+  expect(element.textContent).toBe("Let's edit me!Hello World!")
+  expect(element.tagName).toBe('SPAN')
 })
