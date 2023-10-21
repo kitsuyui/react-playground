8df6cab31417ad29d6a7cf1c3965ac022c9fc442
kitsuyui <kitsuyui@kitsuyui.com>
2023-10-22 01:14:46 +0900

    Add tests to the EditableLabel component onEditComplete event

--- packages/editablelabel/src/test.tsx
+++ packages/editablelabel/src/test.tsx
@@ -4,33 +4,45 @@ import React, { useState } from 'react'
 
 import { EditableLabel } from './index'
 
-const EditableLabelExample = () => {
+const EditableLabelExample = (props: { onEditComplete: () => void }) => {
   const [text, setText] = useState("Let's edit me!")
   return (
     <EditableLabel
       text={text}
-      onEditComplete={(text: string) => setText(text)}
+      onEditComplete={(text: string) => {
+        setText(text)
+        props.onEditComplete()
+      }}
     />
   )
 }
 
 test('render EditableLabel', async () => {
-  render(<EditableLabelExample />)
+  const handleEditComplete = jest.fn()
+  render(<EditableLabelExample onEditComplete={handleEditComplete} />)
+
+  const firstMessage = "Let's edit me!"
+  const additonalMessage = 'Hello World!'
+
   // view mode (initial state)
-  const element = screen.getByText("Let's edit me!")
-  expect(element.textContent).toBe("Let's edit me!")
+  const element = screen.getByText(firstMessage)
+  expect(element.textContent).toBe(firstMessage)
   expect(element.tagName).toBe('SPAN')
 
   // click to edit
   await userEvent.click(element)
-  await screen.findByDisplayValue("Let's edit me!")
-  const element2 = screen.getByDisplayValue("Let's edit me!")
+  await screen.findByDisplayValue(firstMessage)
+  const element2 = screen.getByDisplayValue(firstMessage)
   expect(element2.tagName).toBe('INPUT')
-  await userEvent.type(element2, 'Hello World!')
+  await userEvent.type(element2, additonalMessage)
+  expect(element2).toHaveProperty('value', firstMessage + additonalMessage)
+
   // press enter to submit
   await userEvent.type(element2, '{enter}')
 
   // back to view mode
-  expect(element.textContent).toBe("Let's edit me!Hello World!")
+  expect(element.textContent).toBe(firstMessage + additonalMessage)
   expect(element.tagName).toBe('SPAN')
+  // onEditComplete is called
+  expect(handleEditComplete).toBeCalledTimes(1)
 })
