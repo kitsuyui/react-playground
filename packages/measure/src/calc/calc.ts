86922e242dccb627583b2d87b56e9ee253c740b0
kitsuyui <kitsuyui@kitsuyui.com>
2024-03-21 16:37:49 +0900

    Add tests for calc module

--- packages/measure/src/calc/calc.ts
+++ packages/measure/src/calc/calc.ts
@@ -13,9 +13,9 @@ interface Size {
   height: number
 }
 
-const createPoint = (x: number, y: number): Point => ({ x, y })
-const createLine = (start: Point, end: Point): Line => ({ start, end })
-const createLineByFourValue = (x1: number, y1: number, x2: number, y2: number): Line => createLine(createPoint(x1, y1), createPoint(x2, y2))
+export const createPoint = (x: number, y: number): Point => ({ x, y })
+export const createLine = (start: Point, end: Point): Line => ({ start, end })
+export const createLineByFourValue = (x1: number, y1: number, x2: number, y2: number): Line => createLine(createPoint(x1, y1), createPoint(x2, y2))
 
 export const createCrossLinesBySize = (size: Size): Line[] => {
   const { width, height } = size
