1c94a61823aebe7dc9fd288cec82c82342ee7de4
kitsuyui <kitsuyui@kitsuyui.com>
2025-06-14 21:42:39 +0900

    Refactor: biticon package to use '@kitsuyui/cipher'
    - biticon had many cryptographic functions that were not well tested.
    - I implemented a new package '@kitsuyui/cipher' in https://github.com/kitsuyui/ts-playground
    - Use the new package and remove the old functions from biticon.
    - '@kitsuyui/cipher' is statically bundled in the biticon package using tsup, so it does not require additional dependencies.

---
+++ packages/biticon/tsup.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  target: 'es2020',
+  format: ['cjs', 'esm'],
+  entry: [
+    './src/**/*.{ts,tsx}',
+    '!./src/**/*.test.{ts,tsx}',
+    '!./src/**/test.{ts,tsx}',
+    '!./src/stories/**/*.stories.tsx',
+    '!./src/stories/**/*.mdx',
+  ],
+  loader: {
+    '.css': 'local-css',
+  },
+  sourcemap: true,
+  dts: true,
+  // static link dependencies
+  noExternal: [
+    // Do not include custom utilities in dependencies
+    '@kitsuyui/cipher',
+  ],
+})
