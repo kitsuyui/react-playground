11ee7a5d0ae1af725f2a635141edb8eb98380ca4
kitsuyui <kitsuyui@kitsuyui.com>
2025-06-15 18:56:01 +0900

    Use React 19 style ref handling (No need to use `useRef`)

--- packages/textfield/src/TextArea.tsx
+++ packages/textfield/src/TextArea.tsx
@@ -1,6 +1,5 @@
 import {
-  type ComponentPropsWithoutRef,
-  forwardRef,
+  type ComponentPropsWithRef,
   useCallback,
   useEffect,
   useRef,
@@ -10,7 +9,7 @@ import type React from 'react'
 
 import { useCombinedRefs } from './utils'
 
-type WrappedProps = ComponentPropsWithoutRef<'textarea'>
+type WrappedProps = ComponentPropsWithRef<'textarea'>
 
 type alternateProps = {
   onInputChunk?: (value: string) => void
@@ -27,73 +26,72 @@ type excludeProps =
 
 export type WrapperProps = Omit<WrappedProps, excludeProps> & alternateProps
 
-export const TextArea = forwardRef<HTMLTextAreaElement, WrapperProps>(
-  (props, ref) => {
-    const { onInputChunk, onChangeInputting } = props
-    const [internalValue, setInternalValue] = useState(props.value ?? '')
-    const [isInputting, setIsInputting] = useState(false)
-    const innerRef = useRef<HTMLTextAreaElement>(null)
-    const compbinedRef = useCombinedRefs(innerRef, ref)
+export const TextArea = (props: WrapperProps) => {
+  const ref = props.ref
+  const { onInputChunk, onChangeInputting } = props
+  const [internalValue, setInternalValue] = useState(props.value ?? '')
+  const [isInputting, setIsInputting] = useState(false)
+  const innerRef = useRef<HTMLTextAreaElement>(null)
+  const compbinedRef = useCombinedRefs(innerRef, ref)
 
-    // Use Object.assign({}, props) instead of just { ...props } because it must create deep copy.
-    const {
-      onInputChunk: _1,
-      onChangeInputting: _2,
-      ...propsExcludedWrapperProps
-    } = Object.assign({}, props)
-    const onBlur = propsExcludedWrapperProps.onBlur
+  // Use Object.assign({}, props) instead of just { ...props } because it must create deep copy.
+  const {
+    onInputChunk: _1,
+    onChangeInputting: _2,
+    ...propsExcludedWrapperProps
+  } = Object.assign({}, props)
+  const onBlur = propsExcludedWrapperProps.onBlur
 
-    useEffect(() => {
-      setInternalValue(props.value ?? '')
-    }, [props.value])
+  useEffect(() => {
+    setInternalValue(props.value ?? '')
+  }, [props.value])
 
-    const handleChange = useCallback(() => {
-      const text = innerRef?.current?.value ?? ''
-      setInternalValue(text)
-      if (isInputting) return
-      onInputChunk?.(text)
-    }, [isInputting, onInputChunk])
+  const handleChange = useCallback(() => {
+    const text = innerRef?.current?.value ?? ''
+    setInternalValue(text)
+    if (isInputting) return
+    onInputChunk?.(text)
+  }, [isInputting, onInputChunk])
 
-    const handleCompositionChange = useCallback(() => {
-      const text = innerRef?.current?.value ?? ''
-      setInternalValue(text)
-      setIsInputting(true)
-      onChangeInputting?.(true)
-    }, [onChangeInputting])
+  const handleCompositionChange = useCallback(() => {
+    const text = innerRef?.current?.value ?? ''
+    setInternalValue(text)
+    setIsInputting(true)
+    onChangeInputting?.(true)
+  }, [onChangeInputting])
 
-    const handleCompositionEnd = useCallback(() => {
+  const handleCompositionEnd = useCallback(() => {
+    const text = innerRef?.current?.value ?? ''
+    setInternalValue(text)
+    setIsInputting(false)
+    onChangeInputting?.(false)
+    onInputChunk?.(text)
+  }, [onInputChunk, onChangeInputting])
+
+  const handleOnBlur = useCallback(
+    (e: React.FocusEvent<HTMLTextAreaElement>) => {
       const text = innerRef?.current?.value ?? ''
       setInternalValue(text)
       setIsInputting(false)
       onChangeInputting?.(false)
       onInputChunk?.(text)
-    }, [onInputChunk, onChangeInputting])
+      onBlur?.(e)
+    },
+    [onInputChunk, onChangeInputting, onBlur]
+  )
 
-    const handleOnBlur = useCallback(
-      (e: React.FocusEvent<HTMLTextAreaElement>) => {
-        const text = innerRef?.current?.value ?? ''
-        setInternalValue(text)
-        setIsInputting(false)
-        onChangeInputting?.(false)
-        onInputChunk?.(text)
-        onBlur?.(e)
-      },
-      [onInputChunk, onChangeInputting, onBlur]
-    )
-
-    return (
-      <textarea
-        {...propsExcludedWrapperProps}
-        ref={compbinedRef}
-        value={internalValue}
-        onCompositionStart={handleCompositionChange}
-        onCompositionUpdate={handleCompositionChange}
-        onCompositionEnd={handleCompositionEnd}
-        onBlur={handleOnBlur}
-        onChange={handleChange}
-      />
-    )
-  }
-)
+  return (
+    <textarea
+      {...propsExcludedWrapperProps}
+      ref={compbinedRef}
+      value={internalValue}
+      onCompositionStart={handleCompositionChange}
+      onCompositionUpdate={handleCompositionChange}
+      onCompositionEnd={handleCompositionEnd}
+      onBlur={handleOnBlur}
+      onChange={handleChange}
+    />
+  )
+}
 
 TextArea.displayName = 'TextArea'
--- packages/textfield/src/TextField.tsx
+++ packages/textfield/src/TextField.tsx
@@ -1,6 +1,5 @@
 import {
-  type ComponentPropsWithoutRef,
-  forwardRef,
+  type ComponentPropsWithRef,
   useCallback,
   useEffect,
   useRef,
@@ -10,7 +9,7 @@ import type React from 'react'
 
 import { useCombinedRefs } from './utils'
 
-type WrappedProps = ComponentPropsWithoutRef<'input'>
+type WrappedProps = ComponentPropsWithRef<'input'>
 
 type alternateProps = {
   onInputChunk?: (value: string) => void
@@ -28,74 +27,73 @@ type excludeProps =
 
 export type WrapperProps = Omit<WrappedProps, excludeProps> & alternateProps
 
-export const TextField = forwardRef<HTMLInputElement, WrapperProps>(
-  (props, ref) => {
-    const innerRef = useRef<HTMLInputElement>(null)
-    const { onInputChunk, onChangeInputting } = props
-    const [internalValue, setInternalValue] = useState(props.value ?? '')
-    const [isInputting, setIsInputting] = useState(false)
-    const combinedRef = useCombinedRefs(innerRef, ref)
+export const TextField = (props: WrapperProps) => {
+  const ref = props.ref
+  const { onInputChunk, onChangeInputting } = props
+  const [internalValue, setInternalValue] = useState(props.value ?? '')
+  const [isInputting, setIsInputting] = useState(false)
+  const innerRef = useRef<HTMLInputElement>(null)
+  const combinedRef = useCombinedRefs(innerRef, ref)
 
-    // Use Object.assign({}, props) instead of just { ...props } because it must create deep copy.
-    const {
-      onInputChunk: _1,
-      onChangeInputting: _2,
-      ...propsExcludedWrapperProps
-    } = Object.assign({}, props)
-    const onBlur = propsExcludedWrapperProps.onBlur
+  // Use Object.assign({}, props) instead of just { ...props } because it must create deep copy.
+  const {
+    onInputChunk: _1,
+    onChangeInputting: _2,
+    ...propsExcludedWrapperProps
+  } = Object.assign({}, props)
+  const onBlur = propsExcludedWrapperProps.onBlur
 
-    useEffect(() => {
-      setInternalValue(props.value ?? '')
-    }, [props.value])
+  useEffect(() => {
+    setInternalValue(props.value ?? '')
+  }, [props.value])
 
-    const handleChange = useCallback(() => {
+  const handleChange = useCallback(() => {
+    const text = innerRef?.current?.value ?? ''
+    setInternalValue(text)
+    if (isInputting) return
+    onInputChunk?.(text)
+  }, [isInputting, onInputChunk])
+
+  const handleCompositionChange = useCallback(() => {
+    const text = innerRef?.current?.value ?? ''
+    setInternalValue(text)
+    setIsInputting(true)
+    onChangeInputting?.(true)
+  }, [onChangeInputting])
+
+  const handleCompositionEnd = useCallback(() => {
+    const text = innerRef?.current?.value ?? ''
+    setInternalValue(text)
+    onChangeInputting?.(false)
+    onInputChunk?.(text)
+    setIsInputting(false)
+  }, [onInputChunk, onChangeInputting])
+
+  const handleOnBlur = useCallback(
+    (e: React.FocusEvent<HTMLInputElement>) => {
       const text = innerRef?.current?.value ?? ''
       setInternalValue(text)
-      if (isInputting) return
-      onInputChunk?.(text)
-    }, [isInputting, onInputChunk])
-
-    const handleCompositionChange = useCallback(() => {
-      const text = innerRef?.current?.value ?? ''
-      setInternalValue(text)
-      setIsInputting(true)
-      onChangeInputting?.(true)
-    }, [onChangeInputting])
-
-    const handleCompositionEnd = useCallback(() => {
-      const text = innerRef?.current?.value ?? ''
-      setInternalValue(text)
+      setIsInputting(false)
       onChangeInputting?.(false)
       onInputChunk?.(text)
-      setIsInputting(false)
-    }, [onInputChunk, onChangeInputting])
+      onBlur?.(e)
+    },
+    [onInputChunk, onChangeInputting, onBlur]
+  )
 
-    const handleOnBlur = useCallback(
-      (e: React.FocusEvent<HTMLInputElement>) => {
-        const text = innerRef?.current?.value ?? ''
-        setInternalValue(text)
-        setIsInputting(false)
-        onChangeInputting?.(false)
-        onInputChunk?.(text)
-        onBlur?.(e)
-      },
-      [onInputChunk, onChangeInputting, onBlur]
-    )
-
-    return (
-      <input
-        {...propsExcludedWrapperProps}
-        type="text"
-        ref={combinedRef}
-        value={internalValue}
-        onCompositionStart={handleCompositionChange}
-        onCompositionUpdate={handleCompositionChange}
-        onCompositionEnd={handleCompositionEnd}
-        onBlur={handleOnBlur}
-        onChange={handleChange}
-      />
-    )
-  }
-)
+  return (
+    <input
+      {...propsExcludedWrapperProps}
+      type="text"
+      ref={combinedRef}
+      value={internalValue}
+      onCompositionStart={handleCompositionChange}
+      onCompositionUpdate={handleCompositionChange}
+      onCompositionEnd={handleCompositionEnd}
+      onBlur={handleOnBlur}
+      onChange={handleChange}
+    />
+  )
+}
 
 TextField.displayName = 'TextField'
